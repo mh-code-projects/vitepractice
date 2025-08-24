94d23c43b79e9170cfbd6e27ea82bbebcb34e5f9
funzor <hassonmike@gmail.com>
2025-08-24 13:47:54 -0400

    sync

--- src/QuoteApp.jsx
+++ src/QuoteApp.jsx
@@ -1,10 +1,17 @@
+import { useState, useEffect }from "react";
 
-const [randomQuote, getRandomQuote] = useState(null);
+function fetchRandomQuote() = useEffect(() => { 
+    fetch(`https://api.quotable.io/random`)
+    .then(response => response.json())
+    .then(data => { setRandomQuote(data) } ); 
+    }, []);
 
+const [randomQuote, setRandomQuote] = useState(null);
 useEffect(() => { 
-    fetch(`https://api.quotable.io/random`).then(response => response.json()).then(data => { getRandomQuote(data) } );
-    setVariableFunction
-    return quote;
-}, []);
+    fetch(`https://api.quotable.io/random`)
+    .then(response => response.json())
+    .then(data => { setRandomQuote(data) } ); 
+    }, []);
+return (randomQuote ? (<div> <h1>{randomQuote.content}</h1><p>- {randomQuote.author}</p></div>) : (<p>Loading...</p>));
 
 
