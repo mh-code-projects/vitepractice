3ba804487b9ff1441b66f713e3598510e0875548
funzor <hassonmike@gmail.com>
2025-08-24 20:33:14 -0400

    working version

--- src/QuoteApp.jsx
+++ src/QuoteApp.jsx
@@ -2,17 +2,31 @@ import { useState, useEffect } from "react";
 
 function QuoteApp(){
     const [randomQuote, setRandomQuote] = useState(null);
-
+    const [seconds, setSeconds] = useState(10);
+    
     function getNewQuote() {
         fetch(`https://api.quotable.io/random`)
         .then(response => response.json())
         .then(data => { setRandomQuote(data) } );
 }
 
+    useEffect(() => {  
+        const intervalId = setInterval(() => {
+            setSeconds(prevSeconds => prevSeconds - 1)
+        }, 1000);
+        return () => clearInterval(intervalId);
+        }, []);
 
-useEffect(() => {  
-    getNewQuote()
-    }, []);
+    useEffect(() => {
+        if (seconds === 0) {
+            getNewQuote();
+            setSeconds(10);
+        }
+    }, [seconds]);
+    
+    useEffect(() =>{  
+        getNewQuote()}, []);
+
 
 
 return (randomQuote ? (<div> 
